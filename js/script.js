7db94d31e2b2088152b89c997f4daf5025a712b3
sheblair <66489366+sheblair@users.noreply.github.com>
2022-10-20 18:51:19 -0400

    add function to count guesses remaining, add async function for random words

--- js/script.js
+++ js/script.js
@@ -23,47 +23,69 @@ const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
 // the word the player must guess
-const word = "magnolia";
+let word = "magnolia";
 
 // the empty array to contain all the letters the player has guessed
 const guessedLetters = [];
 
+let remainingGuesses = 8;
+
+const getWord = async function () {
+    const response = await fetch("https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt");
+    const words = await response.text();
+    const wordArray = words.split("\n");
+
+    const randomIndex = Math.floor(Math.random() * wordArray.length);
+    word = wordArray[randomIndex].trim();
+
+    // this is the way i originally coded it
+    // const randomWord = wordArray[randomIndex].trim();
+    // // this wasn't working at first because i declared a new variable ("let word = randomWord") instead of redeclaring
+    // word = randomWord;
+
+    placeholder(word);
+};
 
 const placeholder = function (word) {
-    
     const placeholderLetters = [];
-
     for (let letter of word) {
         console.log(letter);
         placeholderLetters.push("●");
     }
-
     wordInProgress.innerText = placeholderLetters.join("");
 };
 
-placeholder(word);
+getWord();
 
 
 guessLetterButton.addEventListener("click", function (e) {
     e.preventDefault();
-
     // empty message paragraph
     message.innerText = "";
+
     // grab what was entered in the input
-    const guess = letterInput.value;    
+    const guess = letterInput.value;
+
     // making sure it is a single letter
     const goodGuess = validateInput(guess);
 
     // make the guess with the variable that contains the validated input
-     makeGuess(goodGuess);
+    // this is a conditional block that checks to see if goodGuess has returned an input 
+    // (only true if it is a letter that matches the regex), or if the return has been undefined
+    if (goodGuess) {
+        makeGuess(guess);
+    }
+
+    // this is how i was coding it. the problem with this is that while it works, 
+    // it returns an error whenever the user types in an unaccepted character, 
+    // because the program doesn't know what to do with undefined. in the conditional block, 
+    // you're telling it that if it gets undefined, it doesn't try to do anything at all, just stop running the code completely
+    //      makeGuess(goodGuess);
 
     // empty letter input for next guess
-    letterInput.value = "";  
+    letterInput.value = "";
 
-    // this conditional block is the solution code - this doesn't make sense to me
-    // if (goodGuess) {
-    //     makeGuess(guess);
-    // }
+    
 });
 
 const validateInput = function (input) {
@@ -89,13 +111,13 @@ const makeGuess = function (guess) {
         guessedLetters.push(guess);
         console.log(guessedLetters);
         showGuessedLetters();
+        updateGuessesRemaining(guess);
         updateWordInProgress(guessedLetters);
     }
 };
 
 const showGuessedLetters = function () {
     guessedLettersElement.innerHTML = "";
-
     guessedLetters.forEach(function (letter) {
         let li = document.createElement("li");
         li.innerText = letter;
@@ -104,15 +126,18 @@ const showGuessedLetters = function () {
 };
 
 const updateWordInProgress = function (guessedLetters) {
-
     const wordUpper = word.toUpperCase();
     const wordArray = wordUpper.split("");
     const revealWord = [];
-
     // i got everything pretty much on my own except for the else statement, and also i had reversed
     // the loop so was let letter of guessedLetters rather than let letter of wordArray. which DOES matter
-    // bc i just tried reversing it and it fucks it up. honestly this bit was hard for me, i struggled a lot
-    // with it and am still kinda not sure if i totally understand it
+    // bc i just tried reversing it and it fucks it up.
+
+    // it 'knows where to put the letter' because it's looping this every single time you guess!! 
+    // so it deals with the updated revealWord array, not the empty array we declared above
+    // this is something i have to remind myself - the program is running this code every single time we make a guess,
+    // not operating as one big push, but many iterations over and over the same code,
+    // so it starts over at the top every single time. it's filling up the revealWord array as we go. 
     for (let letter of wordArray) {
         if (guessedLetters.includes(letter)) {
             revealWord.push(letter.toUpperCase());
@@ -120,14 +145,34 @@ const updateWordInProgress = function (guessedLetters) {
             revealWord.push("●");
         }
     };
-
     wordInProgress.innerText = revealWord.join("");
     checkIfWin();
+};
+
+const updateGuessesRemaining = function (guess) {
+    // why don't we need to convert word to an array here in order to check if the guess is inside it?
+    // because you can use includes() on both strings and arrays in javascript
+    word = word.toUpperCase();
+    if (!word.includes(guess)) {
+        message.innerText = "That one isn't in the word. Guess again!";
+        remainingGuesses -= 1;
+    } else {
+        message.innerText = "Good guess! That letter is in the word.";
+    }
+
+    if (remainingGuesses === 0) {
+        message.innerText = `Game over! The word was ${word}.`;
+        remainingGuessesSpan.innerText = `${remainingGuesses} guesses`;
+    } else if (remainingGuesses === 1) {
+        remainingGuessesSpan.innerText = `${remainingGuesses} guess`;
+    } else {
+        remainingGuessesSpan.innerText = `${remainingGuesses} guesses`;
+    }
 };
 
 const checkIfWin = function () {
     if (wordInProgress.innerText === word.toUpperCase()) {
         message.classList.add("win");
-        message.innerHTML = `<p class="highlight">You guessed correct the word! Congrats!</p>`;
+        message.innerHTML = `<p class="highlight">You guessed the correct word! Congrats!</p>`;
     }
 };
